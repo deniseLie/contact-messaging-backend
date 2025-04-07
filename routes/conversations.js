d45400d03ef4bda66b9b45982fecb59e8b04a4bd
denise <22denise.lie@gmail.com>
2025-04-07 15:19:45 +0800

    implement query

--- routes/conversations.js
+++ routes/conversations.js
@@ -2,24 +2,88 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../db');
 
-// Get /conversations?page=1
+
+// Get all conversations
 router.get('/', async (req, res) => {
+    const query = `
+        SELECT * 
+        FROM messages
+        LIMIT 10
+    `
+    try {
+        const result = await pool.query(query, [contact_id, offset, limit]);
+        res.json(result.rows);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send('Query failed');
+    }
+})
+
+// Get /conversations?page=1
+router.get('/recent/', async (req, res) => {
+    const contact_id = req.query.id;
     const page = parseInt(req.query.page) || 1;
     const limit = 50;
     const offset = (page - 1) * limit;
 
+    // Query retrieve 50 most recent conversations
     const query = `
+        WITH latest_messages AS (
+            SELECT DISTINCT ON (LEAST(sender_id, receiver_id), GREATEST(sender_id, receiver_id))
+                id,
+                sender_id,
+                receiver_id,
+                content,
+                created_at
+            FROM messages
+            WHERE sender_id = $1 OR receiver_id = $1
+            ORDER BY LEAST(sender_id, receiver_id), GREATEST(sender_id, receiver_id), created_at DESC
+        )
         SELECT *
-        FROM messages m
-    `
+        FROM latest_messages
+        ORDER BY created_at DESC
+        OFFSET $2 LIMIT $3
+    `;
 
     try {
-        const result = await pool.query(query, [offset, limit]);
+        const result = await pool.query(query, [contact_id, offset, limit]);
         res.json(result.rows);
     } catch (e) {
         console.error(e);
         res.status(500).send('Query failed');
     }
 });
+
+router.get('/search', async (req, res) => {
+    const searchValue = req.query.searchValue || '';
+    const page = parseInt(req.query.page) || 1;
+    const limit = 50;
+    const offset = (page - 1) * limit;
+
+    const query = `
+        SELECT 
+            m.*,
+            c.name AS contact_name,
+            c.phone_number AS contact_phone_number
+        FROM messages m
+        JOIN contacts c ON (c.id = m.sender_id OR c.id = m.receiver_id)
+        WHERE (
+            m.content ILIKE $1 OR 
+            c.name ILIKE $1 OR
+            c.phone_number ILIKE $1
+        )
+        ORDER BY m.created_at DESC
+        OFFSET $2 LIMIT $3
+    `;
+
+    try {
+        const result = await pool.query(query, [`%${searchValue}%`, offset, limit]);
+        res.json(result.rows);
+        console.log(result.rows)
+    } catch (e) {
+        console.error(e);
+        res.status(500).send('Query failed');
+    }
+})
 
 module.exports = router;
